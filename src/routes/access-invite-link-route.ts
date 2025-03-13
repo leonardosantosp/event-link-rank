fbb0b46488633559a0c762b7bcd7cf25e12815df
leo <leo040604@gmail.com>
2025-03-13 20:44:20 -0300

    feat: adding the routes /subscription and /invites routes

---
+++ src/routes/access-invite-link-route.ts
@@ -0,0 +1,37 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from '../env'
+import { accessInviteLink } from '../functions/access-invite-link'
+import { redis } from '../redis/client'
+
+export const accessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/invites/:subscriberId',
+    {
+      schema: {
+        summary: 'Acces invite link and redirect user',
+        tags: ['referral'],
+        description: 'test',
+        params: z.object({
+          subscriberId: z.string(),
+        }),
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { subscriberId } = request.params
+
+      await accessInviteLink({ subscriberId })
+
+      const redirectUrl = new URL(env.WEB_URL)
+
+      redirectUrl.searchParams.set('referrer', subscriberId)
+
+      return reply.redirect(redirectUrl.toString(), 302)
+    }
+  )
+}
---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env.js'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route.js'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route.js'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Nlw Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Http server Running')
+})
